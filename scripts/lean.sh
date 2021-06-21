273cbde4fb8c3a412aca33eed3a0fbab1aaa554a
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-06-21 10:47:07 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -44,7 +44,6 @@ git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
 
 # Add luci-app-adguardhome
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome
 
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
